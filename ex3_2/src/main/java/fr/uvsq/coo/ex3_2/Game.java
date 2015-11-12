179e58db28e24dc544cfe51818662f38c500e2b0
uvsq21501608 <meriemb42@gmail.com>
2015-11-12 11:44:45 +0100

    test1

--- ex3_2/src/main/java/fr/uvsq/coo/ex3_2/Game.java
+++ ex3_2/src/main/java/fr/uvsq/coo/ex3_2/Game.java
@@ -1,5 +1,4 @@
-package fr.uvsq.coo.ex3_2;
-import junit.framework.TestCase;
+package ex3_2;
 public class Game 
 {
 	private int pins;
@@ -17,15 +16,6 @@ public class Game
 	public int score ()
 	{
 		return scoreTotale;
-	}
-	public class BowlingGameTest extends TestCase
-	{
-		public void testGutterGame() throws Exception {
-		Game g = new Game(pins,scoreTotale);
-		for (int i=0; i<20; i++)
-		g.roll(0);
-		assertEquals(0, g.score());
-		}
 	}
 	
 }
